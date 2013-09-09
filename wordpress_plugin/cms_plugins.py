a30e206d23e7300072cb7e1b9504ebd7e59b5c81
Rob Beagrie <rob@myserver.Home>
2013-09-09 21:35:35 +0100

    Moved to local wordpress install

--- wordpress_plugin/cms_plugins.py
+++ wordpress_plugin/cms_plugins.py
@@ -2,7 +2,19 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
-from models import Post, WordpressPosts
+from models import WordpressPosts
+from wordpress.models import Post
+import string
+
+def sanitize_string(my_string):
+    return ''.join([ c for c in my_string if c in string.printable ])
+
+def excerptize_string(my_string):
+    words = my_string.split()
+    words = words[:50]
+    words.append(u'...')
+    words = u' '.join(words)
+    return sanitize_string(words)
 
 class WordpressPostsPlugin(CMSPluginBase):
     model = WordpressPosts
@@ -10,7 +22,14 @@ class WordpressPostsPlugin(CMSPluginBase):
 
     def render(self, context, instance, placeholder):
         context['instance'] = instance
-	context['posts'] = Post.objects.filter(site_id=instance.site_id)[:instance.max_posts]
+	posts = Post.objects.filter()[:instance.max_posts]
+        posts = [ {'title':sanitize_string(p.title),
+                   'excerpt':excerptize_string(p.content),
+                   #'date':'this is my date',
+                   'date':p.post_date,
+                  } for p in posts]
+
+	context['posts'] = posts
         return context
 
 plugin_pool.register_plugin(WordpressPostsPlugin)
