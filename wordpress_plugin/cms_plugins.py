6aada7529f6be85f79d0d2b0558236e814021b7d
Rob Beagrie <rob@beagrie.com>
2018-12-30 13:15:26 +0000

    Python3 updates

--- wordpress_plugin/cms_plugins.py
+++ wordpress_plugin/cms_plugins.py
@@ -2,7 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
-from models import WordpressPosts
+from .models import WordpressPosts
 from wordpress.models import Post, Option
 import string
 
@@ -33,7 +33,7 @@ class WordpressPostsPlugin(CMSPluginBase):
 
     def render(self, context, instance, placeholder):
         context['instance'] = instance
-	posts = Post.objects.published()[:instance.max_posts]
+        posts = Post.objects.published()[:instance.max_posts]
         posts = [ {'title':sanitize_string(p.title),
                    'excerpt':excerptize_string(p.content),
                    #'date':'this is my date',
@@ -41,7 +41,7 @@ class WordpressPostsPlugin(CMSPluginBase):
                    'url':get_url(p),
                   } for p in posts]
 
-	context['posts'] = posts
+        context['posts'] = posts
         return context
 
 plugin_pool.register_plugin(WordpressPostsPlugin)
